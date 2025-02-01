47081fa04fed33bb04926f3db3c4cfb20b0a391f
BxsedSosa <donovanxabad@protonmail.com>
2025-02-01 07:21:57 -0800

    updated

--- beginner/login-registration/login-registration/main.cpp
+++ beginner/login-registration/login-registration/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
-int main() {
+#include "../mongo-cred.cpp"
 
+int main() {
+  extern credentials;
+
+  std::cout << credentials.name << '\n';
   std::cout << "hello world'\n";
 
   return 0;
